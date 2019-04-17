b67fccd1e8f618527b067e9b4e60fcc1ca91251b
yun li <747271667@qq.com>
2019-04-17 15:51:58 +0800

    备案

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="blog-footer">
     <p>Blog template built for <a href="http://getbootstrap.com">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
     <p>
-        <a href="#">Back to top</a>
+        <a href="http://www.miitbeian.gov.cn">皖ICP备19006348号-1</a>
     </p>
 </footer>
